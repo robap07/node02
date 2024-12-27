a313ba80b3bc2712ab8f57d2da453f1b82c130f6
robap07 <joserobertomonteirofaria@gmail.com>
2024-12-26 21:20:59 -0300

    tudo

--- printScreen.js
+++ printScreen.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('https://www.netflix.com/login');
+  await page.goto('https://previsao.inmet.gov.br/3101508');
 
   console.log('Abriu o navegador, acessou a Netflix');
   await page.screenshot({ path: 'screenshot.png' });
